ce1bf2683a434a15e0e4751498d043bbe2becef4
Ben Lebherz <git@benleb.de>
2020-05-16 18:55:43 +0200

    do not overwrite builtin stuff

--- miblepy/cli.py
+++ miblepy/cli.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
-from sys import exit
-
 import click
 
-from miblepy import DEFAULT_CONFIG_FILE, DEFAULT_MAX_RETRIES, Miblepy, __name__, __version__, hl
+from miblepy import (
+    DEFAULT_CONFIG_FILE,
+    DEFAULT_MAX_RETRIES,
+    Miblepy,
+    __name__ as miblepy_name,
+    __version__ as miblepy_version,
+    hl,
+)
 
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
@@ -12,7 +17,13 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option("-c", "--config", default=DEFAULT_CONFIG_FILE, type=click.Path(exists=True), help="path to config file")
-@click.option("-r", "--retries", default=DEFAULT_MAX_RETRIES, type=click.IntRange(0, 10, clamp=True), help="how many times we try to get data from a sensor")
+@click.option(
+    "-r",
+    "--retries",
+    default=DEFAULT_MAX_RETRIES,
+    type=click.IntRange(0, 10, clamp=True),
+    help="times we try to get data from a sensor",
+)
 @click.option("--version", default=False, is_flag=True, help=f"show {__name__} version")
 @click.option("-d", "--debug", default=False, is_flag=True, help="enable debug output")
 def cli(config: str, retries: int, version: bool, debug: bool):
@@ -22,7 +33,7 @@ def cli(config: str, retries: int, version: bool, debug: bool):
     """
 
     if version:
-        click.echo(f"{__name__} {hl(__version__)} | https://github.com/benleb/miblepy | @benleb")
+        click.echo(f"{miblepy_name} {hl(miblepy_version)} | https://github.com/benleb/miblepy | @benleb")
         exit(0)
 
     Miblepy(config_file_path=config, retries=retries, debug=debug).go()
